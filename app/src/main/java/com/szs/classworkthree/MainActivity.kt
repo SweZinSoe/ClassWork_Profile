17abe04e4da05d64b4e18e7de0962376879f5cbd
SweZinSoe <swezinsoekachinstate@gmail.com>
2020-01-13 20:55:55 +0630

    Profile

---
+++ app/src/main/java/com/szs/classworkthree/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.szs.classworkthree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
